561dc14ab1257949d94e6e03846669b963338d84
maferlape <maria.lares.26@gmail.com>
2021-07-24 23:29:31 -0400

    Agregando Form de Cadastro

--- src/Components/Cadastro.js
+++ src/Components/Cadastro.js
@@ -1,65 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react'
 import CadastroCss from '../CSS/Cadastro.module.css'
+import logo from '../Img/logo-car-finder.png'
+import { Link } from 'react-router-dom';
 const Cadastro = () => {
+    const [urlFotoPrincipal, setUrlFotoPrincipal] = useState('')
+    const [cidade, setCidade] = useState('')
+    const [uf, setUf] = useState('')
+    const [contato, setContato] = useState('')
+    const [descricao, setDescricao] = useState('')
+    const [marca, setMarca] = useState('')
+    const [modelo, setModelo] = useState('')
+    const [cor, setCor] = useState('')
+    const [ano, setAno] = useState('')
+    const [combustivel, setCombustivel] = useState('')
+    const [transmissao, setTransmissao] = useState('')
+    const [preco, setPreco] = useState('')
+    const [cadastradoPor, setCadastradoPor] = useState('')
+
+    const create = () => {
+        if (!urlFotoPrincipal && !cidade && !uf && !contato && !descricao  && !contato && !marca && !modelo && !cor && !ano && !combustivel && !transmissao && !preco && !cadastradoPor) 
+        return console.error('Campos obrigatórios!')
+        
+        const data ={
+            urlFotoPrincipal, 
+            cidade,
+            uf, 
+            contato,
+            descricao,
+            marca,
+            modelo,
+            cor,
+            ano,
+            combustivel,
+            transmissao,
+            preco,
+            cadastradoPor
+        }
+
+        // const urlBase = 'https://carfinder-toti.herokuapp.com'
+    
+        fetch('https://carfinder-toti.herokuapp.com/cars', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(response =>response.json())
+        .then(data => {console.log(data)})
+        .catch(error=>{console.log(error)})
+    }
+
     return (
         <div className={CadastroCss.container}>
-            <div className={CadastroCss.titulo}>CADASTRO DE VEICULO</div>
-            <form action="">
+            <div className={CadastroCss.headerCadastro}>
+                <img className={CadastroCss.logo} src={logo} alt="logo" />
+                <div className={CadastroCss.titulo}>CADASTRO DE VEÍCULO</div>   
+            </div>
+            <form onSubmit={() => create()}>
                 <div className={CadastroCss.containerForm}>
                     <div className="mb-3">
-                        <label for="cadastradoPor" className="form-label">Cadastrado por:</label>
-                        <input type="texto" className="form-control" id="cadastradoPor" placeholder="" required/> 
+                        <label htmlFor="urlFotoPrincipal" className="form-label">Foto</label>
+                        <input type="url" className="form-control" id="urlFotoPrincipal" placeholder="https://" required value={urlFotoPrincipal} onChange={(event) => setUrlFotoPrincipal(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="urlFotoPrincipal" className="form-label">Foto</label>
-                        <input type="url" className="form-control" id="urlFotoPrincipal" placeholder="https://" required /> 
+                        <label htmlFor="cidade" className="form-label">Cidade</label>
+                        <input type="texto" className="form-control" id="cidade" placeholder="" required value={cidade} onChange={(event) => setCidade(event.target.value)}/> 
                     </div>
                     <div className="mb-3">
-                        <label for="cidade" className="form-label">Cidade</label>
-                        <input type="texto" className="form-control" id="cidade" placeholder="" required /> 
+                        <label htmlFor="uf" className="form-label">UF</label>
+                        <input type="texto" className="form-control" id="uf" placeholder="" required value={uf} onChange={(event) => setUf(event.target.value)}/> 
                     </div>
                     <div className="mb-3">
-                        <label for="uf" className="form-label">UF</label>
-                        <input type="texto" className="form-control" id="uf" placeholder="" required /> 
+                        <label htmlFor="contato" className="form-label">Contato</label>
+                        <input type="texto" className="form-control" id="contato" placeholder="(11) 11111 1111" required value={contato} onChange={(event) => setContato(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="contato" className="form-label">Contato</label>
-                        <input type="texto" className="form-control" id="contato" placeholder="(11) 11111 1111" required /> 
+                        <label htmlFor="descricao" className="form-label">Descrição</label>
+                        <input type="texto" className="form-control" id="descricao" placeholder="Poco uso" required value={descricao} onChange={(event) => setDescricao(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="descricao" className="form-label">descricao</label>
-                        <input type="texto" className="form-control" id="descricao" placeholder="Poco uso" required /> 
+                        <label htmlFor="marca" className="form-label">Marca</label>
+                        <input type="texto" className="form-control" id="marca" placeholder="Jeep" required value={marca} onChange={(event) => setMarca(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="marca" className="form-label">Marca</label>
-                        <input type="texto" className="form-control" id="marca" placeholder="Jeep" required /> 
+                        <label htmlFor="modelo" className="form-label">Modelo</label>
+                        <input type="texto" className="form-control" id="modelo" placeholder="Corolla XLi 1.6 16V 110cv Mec." required value={modelo} onChange={(event) => setModelo(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="modelo" className="form-label">Modelo</label>
-                        <input type="texto" className="form-control" id="modelo" placeholder="Corolla XLi 1.6 16V 110cv Mec." required /> 
+                        <label htmlFor="cor" className="form-label">Cor</label>
+                        <input type="texto" className="form-control" id="cor" placeholder="Vermelho" required value={cor} onChange={(event) => setCor(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="cor" className="form-label">Cor</label>
-                        <input type="texto" className="form-control" id="cor" placeholder="Vermelho" required /> 
+                        <label htmlFor="ano" className="form-label">Ano</label>
+                        <input type="number" className="form-control" id="ano" placeholder="2014" required value={ano} onChange={(event) => setAno(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="ano" className="form-label">Ano</label>
-                        <input type="number" className="form-control" id="ano" placeholder="2014" required /> 
+                        <label htmlFor="combustivel" className="form-label">Combustivel</label>
+                        <input type="texto" className="form-control" id="combustivel" placeholder="Gasolina" required value={combustivel} onChange={(event) => setCombustivel(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="combustivel" className="form-label">Combustibel</label>
-                        <input type="texto" className="form-control" id="combustivel" placeholder="Gasolina" required /> 
+                        <label htmlFor="transmissao" className="form-label">Transmissão</label>
+                        <input type="texto" className="form-control" id="transmissao" placeholder="Automático" required value={transmissao} onChange={(event) => setTransmissao(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="transmissao" className="form-label">Transmissao</label>
-                        <input type="texto" className="form-control" id="transmissao" placeholder="Automático" required /> 
+                        <label htmlFor="preco" className="form-label">Preço</label>
+                        <input type="number" className="form-control" id="preco" placeholder="1000.12" required value={preco} onChange={(event) => setPreco(event.target.value)} /> 
                     </div>
                     <div className="mb-3">
-                        <label for="preco" className="form-label">Preco</label>
-                        <input type="number" className="form-control" id="preco" placeholder="1000.12" required  /> 
+                        <label htmlFor="cadastradoPor" className="form-label">Cadastrado por:</label>
+                        <input type="texto" className="form-control" id="cadastradoPor" placeholder="" required value={cadastradoPor} onChange={(event) => setCadastradoPor(event.target.value)} /> 
                     </div>
                 </div>
-                <button type="submit">Cadastrar</button>
+                <hr />
+                <div className={CadastroCss.containerButtons}>
+                    <button type="submit" className={CadastroCss.buttonRegistrar}>Cadastrar</button>
+                    <Link to="/" className={CadastroCss.buttonCancelar}>Cancelar</Link>
+                </div> 
             </form>
         </div>
     )
