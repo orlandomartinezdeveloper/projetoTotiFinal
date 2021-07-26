96d2a3cfe350e23b834d92ea5c685a6c4931ea64
Orlando Martinez <omjuniormusic@gmail.com>
2021-07-26 14:31:34 -0300

    Codificando

--- src/Components/AppRouter.js
+++ src/Components/AppRouter.js
@@ -3,14 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import Cadastro from './Cadastro'
+import Mostrar from './Mostrar'
+import EditarApagar from './EditarApagar'
 const AppRouter = () => {
 
     return (
         <div>
             <Router>
                 <Switch>
-                    <Route path='/Header'>
-                        <div>Chao</div>
+                    <Route path='/cadastro'>
+                        <Cadastro />
+                    </Route>
+                    <Route path='/mostrarCar'>
+                        <Mostrar />
+                    </Route>
+                    <Route path='/editar'>
+                        <EditarApagar />
                     </Route>
                     <Route path='/Main'>
                         <div>Hola</div>
