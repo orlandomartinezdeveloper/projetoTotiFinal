5e73c3a833882789ff53ad4853684ca62d84fbdd
maferlape <maria.lares.26@gmail.com>
2021-08-11 09:22:07 -0400

    Modificacao do formato money dos card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,13 +3,19 @@ function Card({ item }) {
     const { _id, urlFotoPrincipal, marca, modelo, preco, cidade } = item;
     let imagesCar = [];
     imagesCar.push(urlFotoPrincipal);
+
+    const formatMoney = (num)=>{
+        const number = new Intl.NumberFormat("pt-br").format(num)
+        console.log(number)
+        return number
+    }
     return (
         < div className={"animate__animated animate__fadeInUp " + CarrosTPCSS.card} >
             <div className={CarrosTPCSS.containerImg}><img src={imagesCar} alt="Imagem" className={CarrosTPCSS.img} /></div>
             <div className={CarrosTPCSS.dados}>
                 <div className={CarrosTPCSS.marca}>{marca}</div>
                 <div className={CarrosTPCSS.modelo}>{modelo}</div>
-                <div className={CarrosTPCSS.preco}>R$ {preco}</div>
+                <div className={CarrosTPCSS.preco}>R$ {formatMoney(preco)}</div>
                 <div className={CarrosTPCSS.modelo}>{cidade}</div>
                 <div className={CarrosTPCSS.preButton}><a href={`/mostrarCar/${_id}`}>Ver <i className="fas fa-sign-out-alt"></i></a></div>
             </div>
